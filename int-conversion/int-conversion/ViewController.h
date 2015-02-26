295f947324bf4e0b81007c86e7a264fbcdf4ae94
Kent Humphries <a-khumphries@expedia.com>
2015-02-26 15:23:05 +0000

    Commit an empty Single View Application

---
+++ int-conversion/int-conversion/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  int-conversion
+//
+//  Created by Kent Hotels on 26/02/2015.
+//  Copyright (c) 2015 humphries.kent. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ int-conversion/int-conversion/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  int-conversion
+//
+//  Created by Kent Hotels on 26/02/2015.
+//  Copyright (c) 2015 humphries.kent. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
